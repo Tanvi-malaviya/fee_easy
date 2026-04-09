5010d1ea86edfc530ddba03983ddf9da1b8db563
tanvi <tanvi.sathwarainfotech@gmail.com>
2026-04-09 11:38:15 +0530

    login change

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Web\DashboardController;
 use App\Http\Controllers\Web\InstituteController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
