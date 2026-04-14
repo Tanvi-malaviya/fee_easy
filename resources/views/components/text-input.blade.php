1b5ca3c9540a471d70745cfd8cd88e5a3b5de4c1
tanvi <tanvi.sathwarainfotech@gmail.com>
2026-04-14 10:22:24 +0530

    feat: add reusable UI components and admin layout with global styling and Alpine.js integration

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-xl shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border border-gray-200 bg-gray-50/50 focus:bg-white focus:border-indigo-500 focus:ring-4 focus:ring-indigo-500/10 rounded-xl px-4 py-2 transition-all duration-300 outline-none w-full']) !!}>
